26b552e5e2eb2359a9c0a6d877f727d362a76809
Yazdan abbasi <yabasi891@gmail.com>
2024-12-11 18:22:24 +0330

    Added product controller

--- shop/Shop.Query/obj/Debug/net8.0/Shop.Query.AssemblyInfo.cs
+++ shop/Shop.Query/obj/Debug/net8.0/Shop.Query.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Query")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc65f8856f9495435b0f2f9b8f722f9a41567d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d852620a7d06b560fc45a49fa4796848b21df3a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Query")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Query")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
